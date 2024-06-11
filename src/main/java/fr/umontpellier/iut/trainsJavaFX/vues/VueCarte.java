8a8ce5c6e46ef186a61ce84160bb937242b43fc7
Julien Renaud <julien.renaud01@etu.umontpellier.fr>
2024-06-11 15:48:10 +0200

    Update main.fxml, VueDuJeu, & VueCarte (carte en main)

--- src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueCarte.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueCarte.java
@@ -1,10 +1,16 @@
 package fr.umontpellier.iut.trainsJavaFX.vues;
 
+import fr.umontpellier.iut.trainsJavaFX.GestionJeu;
 import fr.umontpellier.iut.trainsJavaFX.ICarte;
 import fr.umontpellier.iut.trainsJavaFX.mecanique.cartes.Carte;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+
+import java.util.Random;
 
 /**
  * Cette classe représente la vue d'une carte.
@@ -15,12 +21,32 @@ public class VueCarte extends StackPane {
 
     private final ICarte carte;
 
+
     public VueCarte(ICarte carte) {
         this.carte = carte;
+
+        HBox.setHgrow(this, Priority.ALWAYS);
+        setPrefWidth(1000);
+        setMaxHeight(Double.MAX_VALUE);
+
+        setScaleX(1.2);
+        setOnMouseEntered((event) -> setScaleY(1.2));
+        setOnMouseExited((event) -> setScaleY(1));
+
+        setOnMouseClicked(((mouseEvent) -> {
+            GestionJeu.getJeu().joueurCourantProperty().get().uneCarteDeLaMainAEteChoisie(carte.getNom());
+            System.out.println("La carte" + carte.getNom() + "a été choisie");
+        }));
+        int test = new Random().nextInt(100000, 999999);
+        setStyle("-fx-background-color:#"+Integer.toString(test));
     }
 
     public void setCarteChoisieListener(EventHandler<MouseEvent> quandCarteEstChoisie) {
         setOnMouseClicked(quandCarteEstChoisie);
+    }
+
+    public ICarte getCarte() {
+        return carte;
     }
 
 }
--- src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueDuJeu.java
+++ src/main/java/fr/umontpellier/iut/trainsJavaFX/vues/VueDuJeu.java
@@ -1,17 +1,22 @@
 package fr.umontpellier.iut.trainsJavaFX.vues;
 
 import fr.umontpellier.iut.trainsJavaFX.IJeu;
+import fr.umontpellier.iut.trainsJavaFX.IJoueur;
+import fr.umontpellier.iut.trainsJavaFX.mecanique.cartes.Carte;
+import fr.umontpellier.iut.trainsJavaFX.mecanique.cartes.ListeDeCartes;
+import javafx.collections.ListChangeListener;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.shape.Circle;
 
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * Cette classe correspond à la fenêtre principale de l'application.
@@ -27,6 +32,9 @@ public class VueDuJeu extends BorderPane {
     private final IJeu jeu;
     private VuePlateau plateau;
 
+    @FXML
+    private HBox conteneurMainBottom;
+
 
     public VueDuJeu(IJeu jeu) {
         this.jeu = jeu;
@@ -35,14 +43,41 @@ public class VueDuJeu extends BorderPane {
         setLeft(p);
         p.getChildren().add(plateau);
 
-
         try {
-            HBox ConteneurMain = FXMLLoader.load((getClass().getClassLoader().getResource("fxml/main.fxml")));
-
-            setBottom(ConteneurMain);
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/main.fxml"));
+            loader.setController(this);
+            HBox conteneurMain = loader.load();
+            setBottom(conteneurMain);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        creerCartes();
+
+        jeu.joueurCourantProperty().addListener((Observable, ancien, nouveau) -> {
+            conteneurMainBottom.getChildren().clear();
+            creerCartes();
+        });
+
+
+        for (IJoueur joueur : jeu.getJoueurs()) {
+            joueur.mainProperty().addListener(new ListChangeListener<Carte>() {
+                @Override
+                public void onChanged(Change<? extends Carte> change) {
+                    while (change.next()) {
+                        for (Carte carte : change.getRemoved()) {
+                            VueCarte c = trouverBoutonCartes(carte);
+                            if (c!=null) {
+                                conteneurMainBottom.getChildren().remove(c);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+
+
 
 
 
@@ -59,5 +94,23 @@ public class VueDuJeu extends BorderPane {
     }
 
     EventHandler<? super MouseEvent> actionPasserParDefaut = (mouseEvent -> System.out.println("Passer a été demandé"));
+
+    private void creerCartes() {
+        ListeDeCartes mainJoueurCourrant = jeu.joueurCourantProperty().get().mainProperty();
+        for (int i = 0; i < mainJoueurCourrant.size(); i++) {
+            VueCarte carte = new VueCarte(mainJoueurCourrant.get(i));
+            conteneurMainBottom.getChildren().add(carte);
+        }
+    }
+
+    public VueCarte trouverBoutonCartes(Carte carteAtrouver) {
+        for (Node node : conteneurMainBottom.getChildren()) {
+            VueCarte c = (VueCarte) node;
+            if (carteAtrouver.getNom().equals((c.getCarte().getNom()))) {
+                return c;
+            }
+        }
+        return null;
+    }
 
 }
